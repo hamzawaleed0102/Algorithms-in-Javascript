8c382ab7a7b26b333344709b6b3a9c782e9a70e4
Hamza Waleed <hamzawaleed0102@gmail.com>
2021-10-16 15:30:15 +0500

    chore: Apply prettier

--- Algorithms/2. Medium/Max Words in Sentence.js
+++ Algorithms/2. Medium/Max Words in Sentence.js
@@ -1,4 +1,3 @@
-
 // Given a string S return the maximum number of words in a sentence.
 // The text can be divided into sentences by:
 // 1. Splitting it at dots
@@ -7,23 +6,23 @@
 
 // e.g given S="we test coders. give us a try?" function should return 4
 
-const findMaxWordsInSentence=(s='')=>{
-    let maxCount=0;
-    let prevIndex=0;
-    const separators=['?','.','!']
-    for(let i =0; i<s.length; i++){
-      if(separators.includes(s[i])){
-        const substr=s.substring(prevIndex,i)
-        maxCount=Math.max(substr.trim().split(" ").length,maxCount)
-        prevIndex=i+1;
-      }
+const findMaxWordsInSentence = (s = '') => {
+  let maxCount = 0;
+  let prevIndex = 0;
+  const separators = ['?', '.', '!'];
+  for (let i = 0; i < s.length; i++) {
+    if (separators.includes(s[i])) {
+      const substr = s.substring(prevIndex, i);
+      maxCount = Math.max(substr.trim().split(' ').length, maxCount);
+      prevIndex = i + 1;
     }
-    
-    return maxCount;
   }
-  
-  const s1=findMaxWordsInSentence("we test coders. give us a try?");
-  console.log(s1) // 4
-  
-  const s2=findMaxWordsInSentence("Forget CVs..Save time . x x");
-  console.log(s2) // 2
+
+  return maxCount;
+};
+
+const s1 = findMaxWordsInSentence('we test coders. give us a try?');
+console.log(s1); // 4
+
+const s2 = findMaxWordsInSentence('Forget CVs..Save time . x x');
+console.log(s2); // 2
