60ffa7459c91ff3d58b0bda2a0c0cc8aec257489
Hamza Waleed <hamzawaleed0102@gmail.com>
2021-10-29 18:14:30 +0500

    :tada: Add climbing stair problem solution using DP

---
+++ Algorithms/2. Medium/Climbing Stairs.js
@@ -0,0 +1,32 @@
+/*
+    Problem: https://leetcode.com/problems/climbing-stairs
+    You are climbing a staircase. It takes n steps to reach the top.
+    Each time you can either climb 1 or 2 steps. 
+    In how many distinct ways can you climb to the top?
+*/
+const climbStairs = (n) => {
+    let dp = {};
+    let climb = (sum = 0) => {
+      if (dp[sum] !== undefined) {
+        return dp[sum];
+      }
+      let climb1 = 0;
+      let climb2 = 0;
+      if (sum === n) {
+        return 1;
+      } else if (sum > n) {
+        return 0;
+      }
+  
+      climb1 = 0 + climb(sum + 1);
+      climb2 = 0 + climb(sum + 2);
+  
+      return (dp[sum] = climb1 + climb2);
+    };
+  
+    return climb(0);
+  };
+  
+  let ans = climbStairs(4);
+  console.log(`ans`, ans); // 5
+  
