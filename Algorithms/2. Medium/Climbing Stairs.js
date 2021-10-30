15ad5590e20407056ff2d9b46f711271a805692e
Hamza Waleed <hamzawaleed0102@gmail.com>
2021-10-30 18:15:22 +0500

    chore: prettify

--- Algorithms/2. Medium/Climbing Stairs.js
+++ Algorithms/2. Medium/Climbing Stairs.js
@@ -5,28 +5,27 @@
     In how many distinct ways can you climb to the top?
 */
 const climbStairs = (n) => {
-    let cache = {};
-    let climb = (sum = 0) => {
-      if (cache[sum] !== undefined) {
-        return cache[sum];
-      }
-      let climb1 = 0;
-      let climb2 = 0;
-      if (sum === n) {
-        return 1;
-      } else if (sum > n) {
-        return 0;
-      }
-  
-      climb1 = 0 + climb(sum + 1);
-      climb2 = 0 + climb(sum + 2);
-  
-      return (cache[sum] = climb1 + climb2);
-    };
-  
-    return climb(0);
+  let cache = {};
+  let climb = (sum = 0) => {
+    if (cache[sum] !== undefined) {
+      return cache[sum];
+    }
+    let climb1 = 0;
+    let climb2 = 0;
+    if (sum === n) {
+      return 1;
+    } else if (sum > n) {
+      return 0;
+    }
+
+    climb1 = 0 + climb(sum + 1);
+    climb2 = 0 + climb(sum + 2);
+
+    return (cache[sum] = climb1 + climb2);
   };
-  
-  let ans = climbStairs(4);
-  console.log(`ans`, ans); // 5
-  
+
+  return climb(0);
+};
+
+let ans = climbStairs(4);
+console.log(`ans`, ans); // 5
